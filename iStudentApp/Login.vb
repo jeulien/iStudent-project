6cd53fa926b6bb7eab29540e21aa21fd548e4fe4
Jeulien <99721186+jeulien@users.noreply.github.com>
2022-03-15 13:43:52 +0800

    Update Login.vb

--- iStudentApp/Login.vb
+++ iStudentApp/Login.vb
@@ -5,8 +5,7 @@ Imports System.Text
 
 Public Class Login
     Private tripleDes As New TripleDESCryptoServiceProvider
-    'Dim con As New SqlConnection("Data Source=DESKTOP-64RJ8U5;Initial Catalog=iStudentDB;Integrated Security=True;MultipleActiveResultSets=true")
-    Dim con As New SqlConnection("Data Source=SQL5107.site4now.net;Initial Catalog=db_a83e48_istudentdb;User Id=db_a83e48_istudentdb_admin;Password=lula0224;MultipleActiveResultSets=true")
+   
     Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
