68f806a836a2eba766fd7daa59f8e486bd5b94be
Jeulien <99721186+jeulien@users.noreply.github.com>
2022-03-15 13:45:37 +0800

    Update StudentRecords.vb

--- iStudentApp/Forms/StudentRecords.vb
+++ iStudentApp/Forms/StudentRecords.vb
@@ -2,9 +2,6 @@
 Imports System.IO
 Public Class StudentRecords
 
-    'Dim con As New SqlConnection("Data Source=DESKTOP-64RJ8U5;Initial Catalog=iStudentDB;Integrated Security=True;MultipleActiveResultSets=true")
-    Dim con As New SqlConnection("Data Source=SQL5107.site4now.net;Initial Catalog=db_a83e48_istudentdb;User Id=db_a83e48_istudentdb_admin;Password=lula0224;MultipleActiveResultSets=true")
-
     'COLOR PANEL CHANGES
     Private Sub txtSchoolYr_Enter(sender As Object, e As EventArgs) Handles txtSchoolYr.Enter
         panelSchool.BackColor = Color.FromArgb(41, 128, 185)
