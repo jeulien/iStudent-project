604c1cb863670376b9250e4815c2443a1d4078df
Jeulien <99721186+jeulien@users.noreply.github.com>
2022-03-15 13:46:07 +0800

    Update Users.vb

--- iStudentApp/Forms/Users.vb
+++ iStudentApp/Forms/Users.vb
@@ -5,9 +5,7 @@ Imports System.Text
 
 Public Class Users
     Private tripleDes As New TripleDESCryptoServiceProvider
-    'Dim con As New SqlConnection("Data Source=DESKTOP-64RJ8U5;Initial Catalog=iStudentDB;Integrated Security=True;MultipleActiveResultSets=true")
-    Dim con As New SqlConnection("Data Source=SQL5107.site4now.net;Initial Catalog=db_a83e48_istudentdb;User Id=db_a83e48_istudentdb_admin;Password=lula0224;MultipleActiveResultSets=true")
-
+ 
     Private Sub cmbStudNo_Enter(sender As Object, e As EventArgs) Handles cmbStudNo.Enter
         panelStudNo.BackColor = Color.FromArgb(41, 128, 185)
     End Sub
